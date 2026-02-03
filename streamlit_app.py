761324dc129071f3b669295d5e0e0f8650ba231d
dan-lenz-1 <lenzy_25@hotmail.co.uk>
2026-02-03 13:35:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('searc
 ingredients_list = st.multiselect('Choose upto 5 ingredients:'
                                  ,my_dataframe
                                  ,max_selections=5)
+pd_df=my_dataframe.to_pandas()
 
 if ingredients_list:
     ingredients_string = ''
